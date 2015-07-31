3b06b104742213e4b7b40c66d1238bea24413794
John Master <john.s.master@gmail.com>
2015-07-31 12:26:59 -0400

    Add partials for _comment and _form

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,7 @@ class CommentsController < ApplicationController
   def create
     @user = User.first #Will come from session when implemented
     @post = Post.find(params[:post_id])
-    @body = params.require(:comment).permit(:body)
+    @body = params.require(:comment).permit(:body)[:body]
     @comment = @user.comments.create(user: @user, post: @post, body: @body)
     @comment.save
     redirect_to @post
